b2ccaa8613465a9ad5ebc035a05e265929748049
Neta <netaco432@gmail.com>
2022-12-29 13:54:58 +0200

    comment

---
+++ Ex2_1.java
@@ -0,0 +1,32 @@
+import java.lang.String;
+import java.io.File;
+
+public class Ex2_1 {
+
+    public static String[] createTextFiles(int n, int seed, int bound)
+    {
+        String[] fileNames;
+        for (int i = 0; i < n; i++)
+        {
+            //File newFile = new File
+            //fileName[i] =
+        }
+        return fileNames;
+    }
+
+    public static int getNumOfLines(String[] fileNames)
+    {
+
+        return 0;
+    }
+
+    public int getNumOfLinesThreads(String[] fileNames)
+    {
+        return 0;
+    }
+
+    public int getNumOfLinesThreadPool(String[] fileNames)
+    {
+        return 0;
+    }
+}
